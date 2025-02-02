678d7a035b95f24394135800fdaa5682c7ecfcf4
Archanarenuka19 <Archanar1995@gmail.com>
2025-02-02 00:18:52 -0500

    Implemented Extent Report

--- src/test/java/ca/qaguru/tests/InventoryTests.java
+++ src/test/java/ca/qaguru/tests/InventoryTests.java
@@ -6,14 +6,17 @@ import org.testng.annotations.Test;
 public class InventoryTests extends TestBase {
     @Test
     public void inventoryTest1(){
+        logBroswser();
         System.out.println("inventoryTest1");
     }
     @Test
     public void inventoryTest2(){
+        logBroswser();
         System.out.println("inventoryTest2");
     }
     @Test
     public void inventoryTest3(){
+        logBroswser();
         System.out.println("inventoryTest3");
     }
 }
