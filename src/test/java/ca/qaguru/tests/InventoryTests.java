be0f5378830ab95bc4aec53a8f5b877f369eecae
Archanarenuka19 <Archanar1995@gmail.com>
2025-02-02 02:43:50 -0500

    Implemented Optional Annotations

--- src/test/java/ca/qaguru/tests/InventoryTests.java
+++ src/test/java/ca/qaguru/tests/InventoryTests.java
@@ -1,6 +1,7 @@
 package ca.qaguru.tests;
 
 import ca.qaguru.lib.TestBase;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class InventoryTests extends TestBase {
@@ -10,13 +11,26 @@ public class InventoryTests extends TestBase {
         System.out.println("inventoryTest1");
     }
     @Test
+    public void inventoryTest3(){
+        logBroswser();
+        System.out.println("inventoryTest3");
+    }
+    @Test
     public void inventoryTest2(){
         logBroswser();
         System.out.println("inventoryTest2");
     }
-    @Test
-    public void inventoryTest3(){
-        logBroswser();
-        System.out.println("inventoryTest3");
+    @Test(dataProvider = "dpLogin",dataProviderClass = InventoryTests.class)
+    public void LoginTest(String username, String password){
+        System.out.println("Username :"+username+"Password :"+password);
+    }
+    @DataProvider(name="dpLogin")
+    public Object[][] loginData(){ // returns a two diamensional object array
+        return new Object[][]{
+                {"john", "alpha"},
+                {"david", "beta"},
+                {"sara", "gamma"}
+
+        };
     }
 }
